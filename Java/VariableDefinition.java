6f0cafbc2a4cac0660e46a4fbd8aae8a4ec9d5ab
Eray Akyıldız <akyildiz379@gmail.com>
2017-11-15 17:34:44 +0300

    VariableDefinition.java Eklendi.

---
+++ Java/VariableDefinition.java
@@ -0,0 +1,18 @@
+package helloworld;
+
+/*
+ * @author eray
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+
+        int x = 15;
+        String y = "Open Source";
+        float z = 3.14f;
+        System.out.println( x + "\n" + y + "\n" + z );
+
+    }
+
+}
+
