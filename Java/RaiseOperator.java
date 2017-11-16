c1f375ca6bc84c93d107f8ee5252a8ecb058837a
Eray Akyıldız <akyildiz379@gmail.com>
2017-11-16 22:53:02 +0300

    RaiseOperator.java Eklendi.

---
+++ Java/RaiseOperator.java
@@ -0,0 +1,24 @@
+package javaapplication4;
+
+/*
+ * @author eray
+ */
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("A Sample:");
+        int x = 5;
+        int y = x++;
+        System.out.println( " => " + x );
+        System.out.println( " => " + y );
+
+        System.out.println("Sample Two:");
+        int j = 7;
+        int c = ++j;
+        System.out.println( " => " + j  );
+        System.out.println( " => " + c  );
+
+    }
+
+}
