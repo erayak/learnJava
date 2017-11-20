d34701f6a17be204c85ae34730b2fa96c66fb64e
Eray Akyıldız <akyildiz379@gmail.com>
2017-11-20 19:20:10 +0300

    ReductionOperator.java Eklendi.

---
+++ Java/ReductionOperator.java
@@ -0,0 +1,23 @@
+package javaapplication4;
+
+/*
+ * @author eray
+ */
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("A Sample:");
+        int x = 5;
+        int y = x--;
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println("Sample Two:");
+        int z = 5;
+        int j = --x;
+        System.out.println(z);
+        System.out.println(j);
+
+    }
+}
